383458113d6380cc0a0def0b5e6d41fb244dc3f2
Peach <scdzwyxst@gmail.com>
2017-01-23 18:26:03 +0800

    Add delegate to passing data into webview with MochaJSDelegate

--- Sketch Plugin Manager.sketchplugin/Contents/Sketch/lib/Panel.js
+++ Sketch Plugin Manager.sketchplugin/Contents/Sketch/lib/Panel.js
@@ -1,13 +1,15 @@
 @import 'utils.js';
+@import 'MochaJSDelegate.js';
 
-function Panel(context, options) {
+function Panel(context, options, data) {
   var opts = Object.assign({}, {
     x: 0,
     y: 0,
     width: 600,
-    height: 480
+    height: 480,
+    title: context.plugin.name()
   }, options);
-  var webView;
+  var _self = this;
 
   opts.url = utils.path.join(context.scriptPath, '../panel/', options.template + '.html');
 
@@ -23,9 +25,17 @@ function Panel(context, options) {
   this.panel.standardWindowButton(NSWindowCloseButton).setCOSJSTargetFunction(this.close);
 
   // configure the webView
-  webView = WebView.alloc().initWithFrame(NSMakeRect(0, 0, opts.width, opts.height));
-  webView.setMainFrameURL_(opts.url);
-  this.panel.contentView().addSubview(webView);
+  this.webView = WebView.alloc().initWithFrame(NSMakeRect(0, 0, opts.width, opts.height));
+
+  // add delegate to passing data into webview with MochaJSDelegate
+  this.webView.setFrameLoadDelegate_(new MochaJSDelegate({
+    'webView:didFinishLoadForFrame:': (function () {
+      _self.execute('window.init(' + JSON.stringify(data || {}) + ');');
+    })
+  }).getClassInstance());
+
+  this.webView.setMainFrameURL_(opts.url);
+  this.panel.contentView().addSubview(this.webView);
   this.panel.center();
 }
 
@@ -35,4 +45,8 @@ Panel.prototype.close = function () {
 
 Panel.prototype.show = function () {
   NSApp.runModalForWindow(this.panel);
+};
+
+Panel.prototype.execute = function (jsCode) {
+  return this.webView.stringByEvaluatingJavaScriptFromString(jsCode);
 };
