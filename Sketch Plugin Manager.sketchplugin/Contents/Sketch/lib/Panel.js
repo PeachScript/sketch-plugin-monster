d74af35eaa526957f15dc28cc754bd52db2d5746
Peach <scdzwyxst@gmail.com>
2017-01-24 15:59:29 +0800

    Add delegate to get data from webview

--- Sketch Plugin Manager.sketchplugin/Contents/Sketch/lib/Panel.js
+++ Sketch Plugin Manager.sketchplugin/Contents/Sketch/lib/Panel.js
@@ -1,22 +1,22 @@
 @import 'utils.js';
 @import 'MochaJSDelegate.js';
 
-function Panel(context, options, data) {
+function Panel(options, data, callback) {
   var opts = Object.assign({}, {
     x: 0,
     y: 0,
     width: 600,
     height: 480,
-    title: context.plugin.name()
+    title: options.context.plugin.name()
   }, options);
   var _self = this;
 
-  opts.url = utils.path.join(context.scriptPath, '../panel/', options.template + '.html');
+  opts.url = utils.path.join(options.context.scriptPath, '../panel/', options.template + '.html');
 
   // configure the panel
   this.panel = NSPanel.alloc().init();
   this.panel.setFrame_display(NSMakeRect(opts.x, opts.y, opts.width, opts.height + 20), false);
-  this.panel.setTitleVisibility(NSWindowTitleHidden);
+  this.panel.setTitle(opts.title);
   this.panel.setTitlebarAppearsTransparent(true);
   this.panel.standardWindowButton(NSWindowCloseButton);
   this.panel.standardWindowButton(NSWindowMiniaturizeButton).setHidden(true);
@@ -27,11 +27,28 @@ function Panel(context, options, data) {
   // configure the webView
   this.webView = WebView.alloc().initWithFrame(NSMakeRect(0, 0, opts.width, opts.height));
 
-  // add delegate to passing data into webview with MochaJSDelegate
+  // use MochaJSDelegate to exchange data with webview
   this.webView.setFrameLoadDelegate_(new MochaJSDelegate({
-    'webView:didFinishLoadForFrame:': (function () {
-      _self.execute('window.init(' + JSON.stringify(data || {}) + ');');
-    })
+    'webView:didFinishLoadForFrame:': function () {
+      // export a function to receive data from webview
+      _self.execute((function $dispatch(type, dispatchData) {
+        if (dispatchData) {
+          window.$dispatchData = encodeURI(JSON.stringify(dispatchData));
+        }
+        window.location.hash = type;
+      }).toString());
+
+      // passing data into webView
+      _self.execute('window.$initialize && window.$initialize(' + (data ? JSON.stringify(data) : null) + ');');
+
+    },
+    'webView:didChangeLocationWithinPageForFrame:': function () {
+      // read data from webView and passing to callback function
+      var type = NSURL.URLWithString(_self.webView.mainFrameURL()).fragment();
+      var originalData = _self.webView.windowScriptObject().valueForKey("$dispatchData")
+      var dispatchData = originalData ? JSON.parse(decodeURI(originalData)) : null;
+      callback && callback.call(null, type, dispatchData);
+    }
   }).getClassInstance());
 
   this.webView.setMainFrameURL_(opts.url);
@@ -48,5 +65,5 @@ Panel.prototype.show = function () {
 };
 
 Panel.prototype.execute = function (jsCode) {
-  return this.webView.stringByEvaluatingJavaScriptFromString(jsCode);
+  return this.webView.windowScriptObject().evaluateWebScript(jsCode);
 };
