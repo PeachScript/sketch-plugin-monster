518a68b947dc5de29f95cbfefd9f66a9b54c5844
Peach <scdzwyxst@gmail.com>
2017-03-04 01:58:46 +0800

    Modify release script to adapt to external css files

--- release.sh
+++ release.sh
@@ -10,7 +10,7 @@ echo
 
 if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY = "" ]]; then
   echo "Updating version number..."
-  sed "s/\(<script src=\".*?\)\(v.*\)\(\">\)/\1v$VERSION\3/g" "${src_path}panel/shortcuts.html" > .tmp_html
+  sed "s/\([srchef]=\".*?\)\(v.*\)\(\">\)/\1v$VERSION\3/g" "${src_path}panel/shortcuts.html" > .tmp_html
   sed "s/\(\"version\": \"\)\(.*\)\(\"\)/\1$VERSION\3/g" "${src_path}manifest.json" > .tmp_manifest
 
   mv .tmp_html "${src_path}panel/shortcuts.html"
