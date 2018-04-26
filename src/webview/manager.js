0ec99eb3268f155a6a779ec6fa274075f396f4a2
Peach <scdzwyxst@gmail.com>
2018-04-26 21:51:27 +0800

    feat: add shortcut filter to format shortcut keys

--- src/webview/manager.js
+++ src/webview/manager.js
@@ -1,6 +1,20 @@
 import Vue from 'vue';
 import Manager from './components/Manager';
 
+Vue.filter('shortcut', (input = '') => {
+  const mapping = {
+    ctrl: '\u2303',
+    control: '\u2303',
+    alt: '\u2325',
+    option: '\u2325',
+    shift: '\u21E7',
+    cmd: '\u2318',
+    command: '\u2318',
+  };
+
+  return input.split(' ').map(key => mapping[key] || key).join('');
+});
+
 export default new Vue({
   el: '#app',
   render: h => h(Manager),
