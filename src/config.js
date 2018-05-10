ac8dee9a4b622829516d242c5a8d9b2810c891b7
Peach <scdzwyxst@gmail.com>
2018-05-10 18:04:04 +0800

    feat: add i18n configurations

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
 import os from '@skpm/os';
 import path from '@skpm/path';
+import en from './i18n/en.json';
+import zhHans from './i18n/zh-Hans.json';
+import zhHant from './i18n/zh-Hant.json';
 
 export const paths = {
   plugin: path.join(os.homedir(), '/Library/Application Support/com.bohemiancoding.sketch3/Plugins'),
@@ -11,7 +14,14 @@ export const identifiers = {
   nonexistent: 'error.nonexistent',
 };
 
+export const i18n = {
+  en,
+  'zh-Hans': zhHans,
+  'zh-Hant': zhHant,
+};
+
 export default {
   paths,
   identifiers,
+  i18n,
 };
